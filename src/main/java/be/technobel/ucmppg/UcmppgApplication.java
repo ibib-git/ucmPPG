277c1b6507866bec2c2c6376d93b7f40f0487143
Damien <damien.fricot@hotmail.be>
2020-03-17 10:37:52 +0100

    creation project

---
+++ src/main/java/be/technobel/ucmppg/UcmppgApplication.java
@@ -0,0 +1,13 @@
+package be.technobel.ucmppg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcmppgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcmppgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/technobel/ucmppg/UcmppgApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technobel.ucmppg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcmppgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
