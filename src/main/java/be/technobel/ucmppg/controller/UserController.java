be676daae47e6b865934ccc7c8659c49471e3611
Damien <damien.fricot@hotmail.be>
2020-03-24 16:47:28 +0100

    Jira : PPG-3 UserController : sauvegarde en db du nouvel utilisateur UserDTODetails : nouveau DTO reprenant l'ensemble des info d'un nouvel UtilisateurEntity UtilisateurEntity : creation d'un mapper avec userDTORegister

--- src/main/java/be/technobel/ucmppg/controller/UserController.java
+++ src/main/java/be/technobel/ucmppg/controller/UserController.java
@@ -1,8 +1,12 @@
 package be.technobel.ucmppg.controller;
 
+import be.technobel.ucmppg.DAL.Models.UtilisateurEntity;
+import be.technobel.ucmppg.DAL.repositories.UtilisateurRepository;
+import be.technobel.ucmppg.dto.UserDTODetails;
 import be.technobel.ucmppg.dto.UserDTORegister;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @Api(value = "API pour les opérations CRUD sur les utilisateurs")
@@ -11,14 +15,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
+    private final UtilisateurRepository utilisateurRepository;
+
+    public UserController(UtilisateurRepository utilisateurRepository) {
+        this.utilisateurRepository = utilisateurRepository;
+    }
+
     @ApiOperation(value = "Appelé pour l'enregistrement d'un nouvel utilisateur")
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser (@RequestBody UserDTORegister userDTORegister)
+    public ResponseEntity<UserDTODetails> registerUser (@RequestBody UserDTORegister userDTORegister)
     {
-        //TODO Damien : DAL.save
-        System.out.println(userDTORegister.toString());
-
-        return ResponseEntity.ok("200");
+       UserDTODetails userDTODetails = new UserDTODetails(utilisateurRepository.save(new UtilisateurEntity(userDTORegister)));
+        System.out.println(userDTODetails);
+        return ResponseEntity.ok(userDTODetails);
     }
 
 
