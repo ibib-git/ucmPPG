544908dfca99a96006dea63c709e27d70d07c638
bastienhousiaux <bastienhousiaux@yahoo.fr>
2020-03-31 11:31:03 +0200

    push pour pull request

--- src/main/java/be/technobel/ucmppg/bl/dto/projet/ProjetCreationDTO.java
+++ src/main/java/be/technobel/ucmppg/bl/dto/projet/ProjetCreationDTO.java
@@ -10,23 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjetCreationDTO {
-
     private String nom;
     private String description;
-
-
-    //private ProjetDTO projetParDefaut;
-    //private List<String> emailUtilisateur;
-
-//    public ProjetDTO getProjetParDefaut() {
-//        return projetParDefaut;
-//    }
-//
-//    public void setProjetParDefaut(ProjetDTO projetParDefaut) {
-//        this.projetParDefaut = projetParDefaut;
-//    }
-//
-//    public List<String> getEmailUtilisateur() {
-//        return emailUtilisateur;
-//    }
+    private Long idUtilisateur;
 }
